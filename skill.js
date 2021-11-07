a5ed4b74d533c4e5ca36288b0b25b5a74209f405
blackarch0 <shaikhar287@gmail.com>
2021-11-07 18:37:43 +0530

    first commit

---
+++ skill.js
@@ -0,0 +1,12 @@
+Skills .skill-container .skill-bars .progress .js
+{
+    width: "50%"
+}
+Skills .skill-container .skill-bars .progress .js
+{
+    width: "30%"
+}
+Skills .skill-container .skill-bars .progress .js
+{
+    width: "10%"
+}
